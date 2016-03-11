fe12080f714825db9096829eb4d7c402f366762e
NikitaBuyevich <nikita.buyevich@gmail.com>
2016-03-10 20:26:30 -0600

    init commit

--- retro-calculator/ViewController.swift
+++ retro-calculator/ViewController.swift
@@ -7,19 +7,119 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    
+    var runningNumber = "0"
+    var leftValStr = ""
+    var rightValStr = ""
+    var currentOperation: Operation = Operation.Empty
+    var result = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func numberPressed(btn: UIButton!) {
+        playSound()
+      
+      if runningNumber != "0" {
+        runningNumber += "\(btn.tag)"
+      } else {
+        runningNumber = "\(btn.tag)"
+      }
+        outputLbl.text = runningNumber
     }
-
-
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(Operation.Divide)
+    }
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(Operation.Multiply)
+    }
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(Operation.Subtract)
+    }
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(Operation.Add)
+    }
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(currentOperation)
+    }
+    @IBAction func onCLearPressed(sender: AnyObject) {
+      playSound()
+      runningNumber = "0"
+      leftValStr = "0"
+      rightValStr = "0"
+      currentOperation = Operation.Empty
+      result = "0"
+      outputLbl.text = "0"
+    }
+    
+    func processOperation(op: Operation) {
+      playSound()
+      
+      // A user selected an operator, but then selected another opeartor 
+      if currentOperation != Operation.Empty {
+        if runningNumber != "0" {
+          rightValStr = runningNumber
+          runningNumber = "0"
+          
+          if currentOperation == Operation.Multiply {
+            result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+          } else if currentOperation == Operation.Divide {
+            result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+          } else if currentOperation == Operation.Add {
+            result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+          } else if currentOperation == Operation.Subtract {
+            result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+          }
+          
+          leftValStr = result
+          outputLbl.text = result
+        }
+        currentOperation = op
+      
+      } else {
+        // first time operator pressed 
+        leftValStr = runningNumber
+        runningNumber = "0"
+        currentOperation = op
+      }
+    }
+    
+    func playSound() {
+        if btnSound.playing {
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
 }
 
